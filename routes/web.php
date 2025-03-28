eea07232a91b2f1203e9fd11029d0ac1b4943feb
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-28 22:22:13 +0700

    add: shadcn ui

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
+Route::get('/{any}', function () {
     return view('app');
-});
+})->where('any', '.*');
