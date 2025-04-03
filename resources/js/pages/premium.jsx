c813347c50ff697f9a2b68909f218003cc7a404d
shfwnz <ilhamdzaky2007@gmail.com>
2025-04-03 22:30:11 +0700

    style: simplify code

--- resources/js/pages/premium.jsx
+++ resources/js/pages/premium.jsx
@@ -4,17 +4,16 @@ import { Button } from "@/components/ui/button";
 const premium = () => {
     return (
         <div className="flex items-center justify-center">
-            <Button variant="outline">
-                <div className="grid grid-cols-4 gap-4 items-center">
-                    <div>
-                        <span class="material-symbols-outlined font-[20]">
-                            radio_button_unchecked
-                        </span>
-                    </div>
-                    <div className="col-span-2">
-                        <span>Unlimited Hearts/Month</span>
-                    </div>
-                    <div>3</div>
+            <Button
+                variant="outline"
+                className="h-24 rounded-2xl justify-center"
+            >
+                <div className="grid grid-cols-4 gap-4 items-center bg-yellow-400">
+                    <span className="material-symbols-outlined">
+                        radio_button_unchecked
+                    </span>
+                    <span className="col-span-2">Unlimited Hearts/Month</span>
+                    <span>3</span>
                 </div>
             </Button>
         </div>
