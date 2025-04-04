11ba13c4fd4a68095ee8751361ac9d0dc24771a0
shfwnz <ilhamdzaky2007@gmail.com>
2025-04-04 10:24:32 +0700

    add: button state

--- resources/js/pages/premium.jsx
+++ resources/js/pages/premium.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 const premium = () => {
+    const [view, setView] = useState(0);
+
     return (
         <div className="flex items-center justify-center">
             <Button
                 variant="outline"
                 className="h-24 rounded-2xl justify-center"
+                onClick={() => {
+                    setView(1);
+                    console.log("Button 1 clicked");
+                }}
             >
-                <div className="grid grid-cols-4 gap-4 items-center bg-yellow-400">
+                <div className="grid grid-cols-4 gap-4 items-center">
                     <span className="material-symbols-outlined">
-                        radio_button_unchecked
+                        {view === 1
+                            ? "radio_button_checked"
+                            : "radio_button_unchecked"}
                     </span>
                     <span className="col-span-2">Unlimited Hearts/Month</span>
                     <span>3</span>
