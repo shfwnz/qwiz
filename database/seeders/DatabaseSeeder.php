3d2e62cb353d4e1d432e50954d55bbfb94cecb7a
shfwnz <ilhamdzaky2007@gmail.com>
2025-04-08 15:45:13 +0700

    fix: model name typo, seeder import

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\SubscriptionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
