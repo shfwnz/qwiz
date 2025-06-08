9b5ffcad6c714ab5c16c1be1b2d65b863ad66c6a
Noveryan Putra Pamungkas <noveryanpp@gmail.com>
2025-06-08 18:23:18 +0700

    Reapply "filament admin panel + shield setup"
    This reverts commit b71675c0f87d40676d90df5910f14126f15b69ae.

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::get('/', fn () => Inertia::render('home'));
 Route::get('/quiz', fn () => Inertia::render('quiz'));
 Route::get('/leaderboard', fn () => Inertia::render('leaderboard'));
 Route::get('/premium', fn () => Inertia::render('premium'));
+Route::get('/dashboard', fn () => Inertia::render('dashboard'));
