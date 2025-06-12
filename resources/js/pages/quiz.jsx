aede92d511aaf6206c8909d579ee212257c0a202
shfwnz <ilhamdzaky2007@gmail.com>
2025-06-12 17:55:33 +0700

    fix: restore pages

---
+++ resources/js/pages/quiz.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Header from '@/components/layouts/header';
+import SearchBar from '@/components/search-bar';
+import Items from '@/components/items';
+import Footer from '@/components/layouts/footer';
+import ParticlesBackground from '@/components/particle-background';
+import { usePage } from '@inertiajs/react';
+
+export default function Quizzes() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [clicked, setClicked] = useState();
+
+    const { data } = usePage().props;
+
+    return (
+        <div className="container mx-auto w-full">
+            <ParticlesBackground />
+            <div className="relative text-white right-0 left-0 bottom-0 p-4 min-h-screen min-w-full font-bold font-handjet">
+                <div className="relative top-5 space-y-8 h-[200px] md:min-h-[180px] min-w-full z-50">
+                    <Header />
+                    <div className="relative min-w-full md:pl-10">
+                        <h3 className="text-[30px]">Daftar Quizzies</h3>
+                        <SearchBar
+                            searchTerm={searchTerm}
+                            setSearchTerm={setSearchTerm}
+                            clicked={clicked}
+                            setClicked={setClicked}
+                        />
+                    </div>
+                </div>
+
+                <Items data={data} searchTerm={searchTerm} clicked={clicked} />
+
+                <Footer />
+            </div>
+        </div>
+    );
+}
