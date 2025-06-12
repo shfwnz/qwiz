32ff897b1827aee5dfcc809127778aa8adfafede
shfwnz <ilhamdzaky2007@gmail.com>
2025-06-12 10:56:59 +0700

    fix: model equates to migration

--- app/Models/QuestionOption.php
+++ app/Models/QuestionOption.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class QuestionOption extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['question_id', 'option_text', 'is_correct'];
 }
--- app/Models/StudentAnswer.php
+++ app/Models/StudentAnswer.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class StudentAnswer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'quiz_attempt_id',
+        'question_id',
+        'answer_text',
+        'is_correct',
+        'score',
+        'answer_at',
+    ];
 }
