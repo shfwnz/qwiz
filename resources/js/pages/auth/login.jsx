8cc502f36068f3b39f9089967a85737b4753a0cc
shfwnz <ilhamdzaky2007@gmail.com>
2025-06-13 21:24:22 +0700

    fix: remove jwt

--- resources/js/pages/auth/login.jsx
+++ resources/js/pages/auth/login.jsx
@@ -3,27 +3,30 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import {
     Card,
-    CardAction,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
+import ParticleBackground from '@/components/particle-background';
 
 const Login = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 w-full bg-transparent">
-            <Card className="justify-center flex flex-col items-center w-full max-w-md space-y-2 px-8 py-12">
-                <CardHeader className="flex w-full justify-center">
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <ParticleBackground />
+            <Card className="justify-center flex flex-col items-center w-full max-w-md space-y-2 p-12">
+                <CardHeader className="flex justify-center">
                     <CardTitle className="text-2xl">Login</CardTitle>
                 </CardHeader>
                 <CardContent className="gap-2 flex flex-col w-full">
                     <Input placeholder="Email" />
                     <Input placeholder="Password" />
                 </CardContent>
-                <CardFooter className="w-full">
+                <CardFooter className="w-full flex flex-col gap-3">
                     <Button className="w-full">Login</Button>
+                    <div className="w-full">
+                        <a href="/register">Dont have an account?</a>
+                    </div>
                 </CardFooter>
             </Card>
         </div>
