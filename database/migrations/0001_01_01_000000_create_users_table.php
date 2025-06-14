171734a3b841d27d0e2d56ccdf30285a58f240a6
shfwnz <ilhamdzaky2007@gmail.com>
2025-06-14 15:56:43 +0700

    fix: removing method ->change()

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'teacher', 'student'])->change();
+            $table->enum('role', ['admin', 'teacher', 'student']);
             $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
