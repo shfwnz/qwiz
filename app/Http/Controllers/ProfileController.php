3867d197966827b4b0cd4fd6e1c0089a301be2df
shfwnz <ilhamdzaky2007@gmail.com>
2025-06-15 19:36:03 +0700

    fix: get username

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
     public function index()
     {
+        $user = Auth::user();
+
         return Inertia::render('profile', [
-            'auth' => [
-                'user' => Auth::user(),
-                'check' => Auth::check(),
-            ],
+            'user' => $user,
         ]);
     }
 }
--- resources/js/pages/profile.jsx
+++ resources/js/pages/profile.jsx
@@ -168,7 +168,7 @@ function MobileEditProfile({ open, setOpen }) {
     );
 }
 
-const profile = () => {
+const profile = ({ user }) => {
     const pointCounting = useMotionValue(0);
     const quizCounting = useMotionValue(0);
     const pointResult = useTransform(pointCounting, Math.round);
@@ -211,28 +211,26 @@ const profile = () => {
 
                                 <div className=" text-center items-center lg:text-left space-y-6 flex-1 min-w-0">
                                     <div className="flex flex-col md:flex-row md:items-end gap-3">
-                                        <h1 className="text-4xl font-bold text-white">
-                                            Thomas Alva Edison
+                                        <h1 className="text-4xl font-bold text-white capitalize">
+                                            {user.name}
                                         </h1>
-                                        <p className="text-lg text-gray-400">
-                                            Student
+                                        <p className="text-lg text-gray-400 capitalize">
+                                            {user.role}
                                         </p>
                                     </div>
 
                                     <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
                                         <div className="px-4 py-2 rounded-2xl border ">
-                                            <p className="text-sm text-gray-400">
-                                                Email
-                                            </p>
+                                            <p className="text-sm">Email</p>
                                             <p className="text-white font-medium">
-                                                email@gmail.com
+                                                {user.email}
                                             </p>
                                         </div>
 
                                         <div className="px-4 py-2 rounded-2xl border ">
                                             <p className="text-sm">Phone</p>
                                             <p className="text-white font-medium">
-                                                +62 123-8392-2198
+                                                {user.phone}
                                             </p>
                                         </div>
 
