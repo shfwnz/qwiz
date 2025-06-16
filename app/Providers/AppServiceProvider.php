8622259ecad9fc203e1c5f5d4a5353f35e13b07e
shfwnz <ilhamdzaky2007@gmail.com>
2025-06-16 16:16:19 +0700

    fix: minor update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // if(!str_contains(config('app.url'), 'localhost')) {
+        //        \URL::forceScheme('https');
     }
 }
